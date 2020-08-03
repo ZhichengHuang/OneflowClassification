3e2d0af30c1bcff1cffc58f5ed7623accc36d81d
ZhichengHuang <zhichenghzc@gmail.com>
2020-08-03 18:37:56 +0800

    update

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@ python3 of_cnn_train_val.py \
     --num_nodes=1 \
     --train_data_dir="/vqa_dataset_southeastasia/ImageRepresentation/data/ImageNetof/ImageNetof/" \
     --train_data_part_num=40 \
-    --val_data_dir="/vqa_dataset_southeastasia/ImageRepresentation/ImageNetof/ImageNetof/" \
+    --val_data_dir="/vqa_dataset_southeastasia/ImageRepresentation/data/ImageNetof/ImageNetof/" \
     --val_data_part_num=8 \
     --gpu_num_per_node=8 \
     --loss_print_every_n_iter=100 \
